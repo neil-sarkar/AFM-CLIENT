d9a75f7cf317bd644f3375be167929aaf2170133
Arjun Bali <arjun.bali96@gmail.com>
2016-02-01 11:25:25 -0500

    Cleanup in main.cpp

--- qt_quick_app/main.cpp
+++ qt_quick_app/main.cpp
@@ -19,46 +19,49 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
-    PGA* pga = new PGA(1);
-
+    // Thread declarations
     QThread* serial_thread = new QThread();
-    SerialPort* serial_port = new SerialPort();
-    serial_port->moveToThread(serial_thread);
-    QObject::connect(serial_thread, SIGNAL(started()), serial_port, SLOT(scan_for_ports()));
-    serial_thread->start();
-
-    QQuickView view;
-    Motor* motor = new Motor();
-    view.rootContext()->setContextProperty("motor", motor);
-    view.setSource(QUrl(QStringLiteral("qrc:///Motor.qml")));
-    view.show();
-
-    QQuickView view2;
-    DAC* dac = new DAC(1);
-    dac->init();
-    view2.rootContext()->setContextProperty("dac", dac);
-    view2.setSource(QUrl(QStringLiteral("qrc:///DAC.qml")));
-
-    view2.rootContext()->setContextProperty("serial_port", serial_port);
-    view2.show();
-
     QThread* sender_thread = new QThread();
-    SendWorker* send_worker = new SendWorker();
-    send_worker->moveToThread(sender_thread);
-    sender_thread->start();
-
     QThread* receiver_thread = new QThread();
-    ReceiveWorker* receive_worker = new ReceiveWorker();
-    receive_worker->moveToThread(receiver_thread);
-    receiver_thread->start();
 
+    // Object creation (to be moved into application builder class
+    Motor* motor = new Motor();
+    DAC* dac = new DAC(1);
+    PGA* pga = new PGA(1);
+    SerialPort* serial_port = new SerialPort();
+    SendWorker* send_worker = new SendWorker();
+    ReceiveWorker* receive_worker = new ReceiveWorker();
+
+    // View config
+    QQuickView motor_view, dac_view;
+    motor_view.rootContext()->setContextProperty("motor", motor);
+    motor_view.setSource(QUrl(QStringLiteral("qrc:///Motor.qml")));
+    motor_view.show();
+    dac->init();
+    dac_view.rootContext()->setContextProperty("dac", dac);
+    dac_view.setSource(QUrl(QStringLiteral("qrc:///DAC.qml")));
+    dac_view.show();
+
+    // Wire up signals and slots
     QObject::connect(motor, SIGNAL(command_generated(CommandNode*)), send_worker, SLOT(enqueue_command(CommandNode*)), Qt::DirectConnection);
     QObject::connect(dac, SIGNAL(command_generated(CommandNode*)), send_worker, SLOT(enqueue_command(CommandNode*)), Qt::DirectConnection);
     QObject::connect(serial_port, SIGNAL(message_sent(CommandNode*)), receive_worker, SLOT(enqueue_command(CommandNode*)), Qt::DirectConnection);
     QObject::connect(serial_port, SIGNAL(byte_received(char)), receive_worker, SLOT(enqueue_response_byte(char)), Qt::DirectConnection);
-
     QObject::connect(send_worker, SIGNAL(command_dequeued(CommandNode*)), serial_port, SLOT(execute_command(CommandNode*)));
+    QObject::connect(serial_thread, SIGNAL(started()), serial_port, SLOT(scan_for_ports()));
 
+
+    // Assign objects to threads
+    serial_port->moveToThread(serial_thread);
+    send_worker->moveToThread(sender_thread);
+    receive_worker->moveToThread(receiver_thread);
+
+    // Thread start
+    serial_thread->start();
+    sender_thread->start();
+    receiver_thread->start();
+
+    // Cleanup
     engine.quit();
     int r = app.exec();
     serial_thread->quit();
