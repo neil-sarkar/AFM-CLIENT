191a58962dda1ade823e12ab75c9192b72442d16
Arjun Bali <arjun.bali96@gmail.com>
2016-02-13 23:33:26 -0500

    Rendering composite components

--- assets/js/pages/app.jsx
+++ assets/js/pages/app.jsx
@@ -1,1 +1,9 @@
-console.log("here");
+define(["jquery", "react", "dom", "jsx!pages/sidebar"], function($, React, ReactDOM, Sidebar){
+	var X = React.createClass({
+		render: function() {
+			return (<div><Sidebar />
+				<div>l</div></div>)
+		}
+	});
+	ReactDOM.render(<X />, document.getElementById('content'));
+});
