b98823233c006855e459183b4ca8ce85fdb5f71d
Arjun Bali <arjun.bali96@gmail.com>
2016-04-21 13:01:35 -0400

    Remove unnecessary max/min calculations

--- qt_quick_app/scan_data.cpp
+++ qt_quick_app/scan_data.cpp
@@ -105,20 +105,30 @@ QColor get_color(double percent) {
 QString ScanData::generate_png() {
      double percentage;
      QColor color;
-     int num_points = size();
 
      // Step 1: Compute the max and the min of the leveled image
      double scan_max = -2 * ADC::RESOLUTION, scan_min = 2 * ADC::RESOLUTION;
-     for (int i = 0; i < raw_data.size(); i++) {
-         if (raw_data[i]->size > 0) {
-            if (raw_data[i]->max - raw_data[i]->average > scan_max) {
-                scan_max = raw_data[i]->max - raw_data[i]->average;
-            }
-            if (raw_data[i]->min - raw_data[i]->average < scan_min) {
-                scan_min = raw_data[i]->min - raw_data[i]->average;
-            }
+
+     // Find the max and min of the lines we've yet draw -- assumes that we add lines to the raw_data sequentially (index 0 to n, not the other way)
+    for (int i = raw_data.size() - 1; i >= 0; i--) {
+        if (raw_data[i]->drawn)
+            break;
+        if (raw_data[i]->size == 0)
+            continue;
+        if (raw_data[i]->max - raw_data[i]->average > scan_max) {
+            scan_max = raw_data[i]->max - raw_data[i]->average;
         }
-     }
+        if (raw_data[i]->min - raw_data[i]->average < scan_min) {
+            scan_min = raw_data[i]->min - raw_data[i]->average;
+        }
+    }
+
+    // Find the extreme max and min for the entire scan ( would probably cause an issue with the first line because prev min is set to -2*4095 and prev max is 2*4095)
+    if (scan_max < m_prev_max)
+        scan_max = m_prev_max;
+    if (scan_min > m_prev_min)
+        scan_min = m_prev_min;
+
      bool same_range = (m_prev_max == scan_max && m_prev_min == scan_min);
 
      for (int i = 0; i < m_num_rows; i++) {
@@ -142,8 +152,8 @@ QString ScanData::generate_png() {
 
          raw_data[i]->drawn = true;
      }
-     m_prev_min = scan_max;
-     m_prev_max = scan_min;
+     m_prev_min = scan_min;
+     m_prev_max = scan_max;
 
      QByteArray ba;
      QBuffer buffer(&ba);
