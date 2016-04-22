ce850a4271023f967b85132efa989972ee44c6c1
Arjun Bali <arjun.bali96@gmail.com>
2016-04-22 15:41:36 -0400

    Removed old gui code

--- gui/CodeValet.bat
+++
@@ -1,9 +0,0 @@
-@ECHO off
-python -m cogapp -e -r -c afm.h
-python -m cogapp -e -r receive_worker.cpp
-python -m cogapp -e -r afm_data.h
-ECHO ----- Completed -----
-ECHO If some files were not updated, try again in a few minutes.
-ECHO Google Drive may not have updated the CSV yet
-ECHO ---------------------
-pause
--- gui/debugger.h
+++
@@ -1,4 +0,0 @@
-#ifndef DEBUGGER_H
-#define DEBUGGER_H
-void debug(int num_args, bool input, ...);
-#endif // DEBUGGER_H
