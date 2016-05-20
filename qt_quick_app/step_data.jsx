be891373a15c5101610e7b42db0722d5218f4cb6
Andrew Yang <andrewafy@gmail.com>
2016-05-20 02:49:38 -0400

    bugfix: show the setup page on start

--- qt_quick_app/step_data.jsx
+++ qt_quick_app/step_data.jsx
@@ -1,12 +1,12 @@
 define(["react", "jsx!pages/setup", "jsx!pages/sweep", "jsx!pages/approach", "jsx!pages/scan", "jsx!pages/null_step"], function(React, Setup, Sweep, Approach, Scan, NullStep) {
 	var StepData = React.createClass({
 		show_first_step: function() {
-			$('#first_step').show();
+                    this.refs.first_step.componentWillReceiveProps(this.refs.first_step.props);
 		},
 		render: function() {
 			return (
                                 <div id="step-data">
-					<Setup ref="first_step" showStep={this.props.step == 0} 
+                                        <Setup ref="first_step" showStep={this.props.step == 0}
 						   go_to_next_step={this.props.go_to_next_step} />
 					<Sweep 	showStep={this.props.step == 1} 
 							go_to_next_step={this.props.go_to_next_step}
@@ -20,7 +20,7 @@ define(["react", "jsx!pages/setup", "jsx!pages/sweep", "jsx!pages/approach", "js
 					<NullStep showStep={this.props.step == 4} 
 						   	go_to_previous_step={this.props.go_to_previous_step} />
 				</div>
-			)	
+                        )
 		},
 	});
 	return StepData;
