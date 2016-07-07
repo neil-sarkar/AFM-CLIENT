8ee8e6433f8f03c73d5f02bffaeb7f4a7a0b777d
Andrew Yang <andrewafy@gmail.com>
2016-07-06 23:59:32 -0400

    Changes to inline scan controls: - Remove send-back-count - Remove unimplemented outliers feature - Add coarse Z control - Add PID controls

--- qt_quick_app/inline_scan_controls.jsx
+++ qt_quick_app/inline_scan_controls.jsx
@@ -1,36 +1,24 @@
-define(["react", "jsx!pages/number_input", "jsx!pages/dropdown", "jsx!pages/text_input", "jsx!pages/pga_controller", "jsx!pages/scan_range_slider"], function(React, NumberInput, Dropdown, TextInput, PGAController, ScanRangeSlider) {
+define(["react", "jsx!pages/number_input", "jsx!pages/dropdown", "jsx!pages/text_input", "jsx!pages/pga_controller", "jsx!pages/dac_controller", "jsx!pages/scan_range_slider", "jsx!pages/pid"], function(React, NumberInput, Dropdown, TextInput, PGAController, DACController, ScanRangeSlider, PIDControl) {
 	var InlineScanControls = React.createClass({
 		render: function() {
 			return (
-				<div>
-					<NumberInput value_type="scan" 
-								name="Send Back Count"
-								min={0}
-								max={1024}
-								step={1} 
-								notify_signal={scanner.send_back_count_changed} 
-								get_value={scanner.send_back_count} 
-								set_value={scanner.set_send_back_count} />
-					<NumberInput value_type="scan" 
-								name="Remove outliers farther than N RMS"
-								min={0}
-								max={100}
-								step={0.01} 
-								notify_signal={scanner.rms_threshold_changed} 
-								get_value={scanner.rms_threshold}
-								set_value={scanner.set_rms_threshold} />
-                                        <Dropdown options_list={[{text: "0:1", cmd_number: 1}, {text: "1:1", cmd_number: 2}, {text: "3:1", cmd_number: 4}, {text: "7:1", cmd_number: 8}]}
-                                                  selection_method={scanner.set_ratio}
-                                                  notify_signal={scanner.ratio_changed}
-                                                  get_value={scanner.ratio}
-                                                  title="Scan ratio"/>
-                                        <Dropdown options_list={[{text: "Forward", cmd_number: 'f'}, {text: "Reverse", cmd_number: 'b'}]}
-                                                  selection_method={scanner.set_leveling_direction}
-                                                  notify_signal={scanner.leveling_direction_changed}
-                                                  get_value={scanner.leveling_direction}
-                                                  title="Leveling direction" />
-					<PGAController name="Leveling" qt_object={pga_leveling}/>
-					<ScanRangeSlider name="Scan Range" qt_objects={[pga_x_1, pga_x_2, pga_y_1, pga_y_2]} />
+                                <div className="top-row">
+                                        <div>
+                                                <Dropdown options_list={[{text: "0:1", cmd_number: 1}, {text: "1:1", cmd_number: 2}, {text: "3:1", cmd_number: 4}, {text: "7:1", cmd_number: 8}]}
+                                                          selection_method={scanner.set_ratio}
+                                                          notify_signal={scanner.ratio_changed}
+                                                          get_value={scanner.ratio}
+                                                          title="Scan ratio"/>
+                                                <Dropdown options_list={[{text: "Forward", cmd_number: 'f'}, {text: "Reverse", cmd_number: 'b'}]}
+                                                          selection_method={scanner.set_leveling_direction}
+                                                          notify_signal={scanner.leveling_direction_changed}
+                                                          get_value={scanner.leveling_direction}
+                                                          title="Leveling direction" />
+                                                <PGAController name="Leveling" qt_object={pga_leveling}/>
+                                                <DACController name="Z Coarse" qt_object={dac_z_offset_coarse}/>
+                                                <ScanRangeSlider name="Scan Range" qt_objects={[pga_x_1, pga_x_2, pga_y_1, pga_y_2]} />
+                                        </div>
+                                        <PIDControl />
 				</div>
 			)
 		}
