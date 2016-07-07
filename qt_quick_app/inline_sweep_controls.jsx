aab03d73dc6f2d925a629826b5975db5ca22e079
Andrew Yang <andrewafy@gmail.com>
2016-07-07 00:01:40 -0400

    Add DDS gain to inline sweep control

--- qt_quick_app/inline_sweep_controls.jsx
+++ qt_quick_app/inline_sweep_controls.jsx
@@ -1,4 +1,4 @@
-define(["react", "jsx!pages/number_input", "jsx!pages/dropdown"], function(React, NumberInput, Dropdown) {
+define(["react", "jsx!pages/number_input", "jsx!pages/dropdown", "jsx!pages/pga_controller"], function(React, NumberInput, Dropdown, PGAController) {
 	var InlineSweepControls = React.createClass({
 		getInitialState: function() {
                         return {num_steps: Math.floor(sweeper.calculate_num_steps())};
@@ -11,32 +11,37 @@ define(["react", "jsx!pages/number_input", "jsx!pages/dropdown"], function(React
 		},
 		render: function() {
 			return (
-				<div>
-					<NumberInput value_type="sweep"
-								name="Start frequency (Hz)"
-								min={0}
-								max={100000}
-								step={1}
-								notify_signal={sweeper.start_frequency_changed}
-								get_value={sweeper.start_frequency}
-								set_value={sweeper.set_start_frequency} />
-					<NumberInput value_type="sweep"
-								name="Stop frequency (Hz)"
-								min={0}
-								max={100000}
-								step={1}
-								notify_signal={sweeper.end_frequency_changed}
-								get_value={sweeper.end_frequency}
-								set_value={sweeper.set_end_frequency} />
-					<NumberInput value_type="sweep" 
-								name="Step size (Hz)"
-								min={1}
-								max={300}
-								step={1}
-								notify_signal={sweeper.step_size_changed}
-								get_value={sweeper.step_size} 
-								set_value={sweeper.set_step_size} />
-					<p style={{marginTop: 0.3}}>Total number of steps in sweep: <span>{this.state.num_steps}</span></p>
+                                <div className="top-row">
+                                        <div>
+                                                <NumberInput value_type="sweep"
+                                                                        name="Start frequency (Hz)"
+                                                                        min={0}
+                                                                        max={100000}
+                                                                        step={1}
+                                                                        notify_signal={sweeper.start_frequency_changed}
+                                                                        get_value={sweeper.start_frequency}
+                                                                        set_value={sweeper.set_start_frequency} />
+                                                <NumberInput value_type="sweep"
+                                                                        name="Stop frequency (Hz)"
+                                                                        min={0}
+                                                                        max={100000}
+                                                                        step={1}
+                                                                        notify_signal={sweeper.end_frequency_changed}
+                                                                        get_value={sweeper.end_frequency}
+                                                                        set_value={sweeper.set_end_frequency} />
+                                                <NumberInput value_type="sweep"
+                                                                        name="Step size (Hz)"
+                                                                        min={1}
+                                                                        max={300}
+                                                                        step={1}
+                                                                        notify_signal={sweeper.step_size_changed}
+                                                                        get_value={sweeper.step_size}
+                                                                        set_value={sweeper.set_step_size} />
+                                                <p style={{marginTop: 0.3}}>Total number of steps in sweep: <span>{this.state.num_steps}</span></p>
+                                        </div>
+                                        <div>
+                                                <PGAController name="DDS Gain" qt_object={pga_dds}/>
+                                        </div>
 				</div>
 			)
 		}
